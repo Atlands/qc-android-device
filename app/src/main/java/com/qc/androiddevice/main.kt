da30053cae6f7c6cd58ddc5f11a987fd17ef6c37
atlands <atlands@qq.com>
2023-07-03 22:59:14 +0800

    test

---
+++ app/src/main/java/com/qc/androiddevice/main.kt
@@ -0,0 +1,9 @@
+package com.qc.androiddevice
+
+import android.content.Context
+import androidx.activity.ComponentActivity
+import com.qc.device.DataCenter
+
+fun test(context: Context){
+    DataCenter(context as ComponentActivity)
+}
